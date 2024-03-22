bd72de841feb54b0d85cdd54fa51e9b766148a8b
Vadim Savosko <99212100+savvadim@users.noreply.github.com>
2024-03-22 17:52:55 +0300

    Initial commit

---
+++ Instagram/Instagram/Feed/FeedPostItemInfo.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+struct FeedPostItemInfo {
+    let userImage: UIImage
+    let username: String
+    let postSubtitle: String
+    let postImage: UIImage
+    let numberOfLikes: Int
+    let comment: CommentShortInfo?
+}
+
+struct CommentShortInfo {
+    let username: String
+    let commentText: String
+}
---
+++ Instagram/Instagram/Feed/FeedStoriesCellInfo.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+struct FeedStoriesItemCellInfo {
+    let image: UIImage
+    let username: String
+    let isAddButtonVisible: Bool
+    let isNewStory: Bool
+}
+
+typealias FeedStoriesCellInfo = [FeedStoriesItemCellInfo]
